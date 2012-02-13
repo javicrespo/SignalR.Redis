17142d6511a584cf3b35ec681feffa9c0549a50f
David Fowler <davidfowl@gmail.com>
2012-02-12 19:07:26 -0800

    Added extension methods on IDependencyResolver to register SignalR.

---
+++ SignalR.Redis/DependencyResolverExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SignalR.Infrastructure;
+using SignalR.MessageBus;
+
+namespace SignalR.Redis
+{
+    public static class DependencyResolverExtensions
+    {
+        public static void UseRedis(IDependencyResolver resolver, string server, int port, string password, string eventKey)
+        {
+            UseRedis(resolver, server, port, password, db: 0, eventKey: eventKey);
+        }
+
+        public static void UseRedis(IDependencyResolver resolver, string server, int port, string password, int db, string eventKey)
+        {
+            resolver.Register(typeof(IMessageBus), () => new RedisMessageBus(server, port, password, db, eventKey, resolver));
+        }
+    }
+}
