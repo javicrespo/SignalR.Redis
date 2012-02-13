c32f1def4ed4a7a039bb0b2a8774f1d7897fa13f
David Fowler <davidfowl@gmail.com>
2012-02-13 01:55:00 -0800

    Make configuration methods extension methods

--- SignalR.Redis/DependencyResolverExtensions.cs
+++ SignalR.Redis/DependencyResolverExtensions.cs
@@ -6,12 +6,12 @@ namespace SignalR.Redis
 {
     public static class DependencyResolverExtensions
     {
-        public static IDependencyResolver UseRedis(IDependencyResolver resolver, string server, int port, string password, string eventKey)
+        public static IDependencyResolver UseRedis(this IDependencyResolver resolver, string server, int port, string password, string eventKey)
         {
             return UseRedis(resolver, server, port, password, db: 0, eventKey: eventKey);
         }
 
-        public static IDependencyResolver UseRedis(IDependencyResolver resolver, string server, int port, string password, int db, string eventKey)
+        public static IDependencyResolver UseRedis(this IDependencyResolver resolver, string server, int port, string password, int db, string eventKey)
         {
             var bus = new Lazy<RedisMessageBus>(() => new RedisMessageBus(server, port, password, db, eventKey, resolver));
             resolver.Register(typeof(IMessageBus), () => bus.Value);
